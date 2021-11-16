2b902c955441945607b3263cd2e4abe756bc5300
Rut Kramer <rut@leader.codes>
2021-11-16 21:16:39 +0200

    changetomulti

--- src/list.js
+++ src/list.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDrag, useDrop } from "react-dnd";
 
 function MovableItem({ color, index }) {
@@ -12,28 +12,41 @@ function MovableItem({ color, index }) {
     )
 }
 
-function List() {
-    const [clr, setClr] = useState("white")
+function Dropable({ color }) {
+    const [clr, setClr] = useState(color)
     const [, drop] = useDrop({
         accept: 'COLOR',
         drop: (item) => (setClr(item.color)),
         collect: monitor => ({
         })
     })
+    useEffect(() => {
+        setClr(color);
+    },[color])
+    return <div ref={drop} className="colorD"
+        style={{ backgroundColor:  clr }}>
+    </div>
+}
 
+function List() {
+    const [cl, setCl] = useState("white")
     return (
         <div>
-
             <div className="container">
                 <div className="colorsList">
                     <MovableItem color="red" index={1} />
                     <MovableItem color="orange" index={2} />
                     <MovableItem color="green" index={3} />
                     <MovableItem color="blue" index={4} />
-                    <button onClick={() => setClr("white")}>clear</button>
+                    <button onClick={() => setCl(cl==="white"?"#ffffff":"white")}>clear</button>
                 </div>
-                <div ref={drop} className="colors"
-                    style={{ backgroundColor: clr }}></div>
+                <div className="colors">
+                    <Dropable color={cl}/>
+                    <Dropable color={cl}/>
+                    <Dropable color={cl}/>
+                    <Dropable color={cl}/>
+                    <Dropable color={cl}/>
+                </div>
             </div>
         </div>
     );
